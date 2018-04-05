7a0b716c6e1a1d4c407fb25b74cd78fb757e7115
Melissa Meyer <mmeyer@articulate.com>
2018-04-05 12:51:25 +1000

    move create & update into functions

--- okta/policies.go
+++ okta/policies.go
@@ -16,10 +16,10 @@ func resourcePolicies() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"type": &schema.Schema{
-				Type:        schema.TypeString,
-				Required:    true,
+				Type:         schema.TypeString,
+				Required:     true,
 				ValidateFunc: validation.StringInSlice([]string{"OKTA_SIGN_ON", "PASSWORD", "MFA_ENROLL", "OAUTH_AUTHORIZATION_POLICY"}, false),
-				Description: "Policy Type: OKTA_SIGN_ON, PASSWORD, MFA_ENROLL, or OAUTH_AUTHORIZATION_POLICY",
+				Description:  "Policy Type: OKTA_SIGN_ON, PASSWORD, MFA_ENROLL, or OAUTH_AUTHORIZATION_POLICY",
 			},
 			"name": &schema.Schema{
 				Type:        schema.TypeString,
@@ -43,78 +43,37 @@ func resourcePolicies() *schema.Resource {
 				Description: "Policy Priority",
 			},
 			"status": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "ACTIVE",
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "ACTIVE",
 				ValidateFunc: validation.StringInSlice([]string{"ACTIVE", "INACTIVE"}, false),
-				Description: "Policy Status: ACTIVE or INACTIVE",
+				Description:  "Policy Status: ACTIVE or INACTIVE",
 			},
 			"conditions": {
-				Type:        schema.TypeSet,
+				Type:        schema.TypeList,
 				Optional:    true,
 				Description: "Conditions that must be met during Policy Evaluation",
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"users": {
-							Type:          schema.TypeSet,
-							Optional:      true,
-							Description:   "List of Users to be Included or Excluded in the Policy",
-							ConflictsWith: []string{"conditions.groups"},
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"include": {
-										Type:          schema.TypeList,
-										Optional:      true,
-										Description:   "List of User IDs to Include",
-										ConflictsWith: []string{"conditions.users.exclude"},
-										Elem:          &schema.Schema{Type: schema.TypeString},
-									},
-									"exclude": {
-										Type:          schema.TypeList,
-										Optional:      true,
-										Description:   "List of User IDs to Exclude",
-										ConflictsWith: []string{"conditions.users.include"},
-										Elem:          &schema.Schema{Type: schema.TypeString},
-									},
-								},
-							},
-						},
 						"groups": {
-							Type:          schema.TypeSet,
-							Optional:      true,
-							Description:   "List of Groups to be Included or Excluded in the Policy",
-							ConflictsWith: []string{"conditions.users"},
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"include": {
-										Type:          schema.TypeList,
-										Optional:      true,
-										Description:   "List of Group IDs to Include",
-										ConflictsWith: []string{"conditions.groups.exclude"},
-										Elem:          &schema.Schema{Type: schema.TypeString},
-									},
-									"exclude": {
-										Type:          schema.TypeList,
-										Optional:      true,
-										Description:   "List of Group IDs to Exclude",
-										ConflictsWith: []string{"conditions.groups.include"},
-										Elem:          &schema.Schema{Type: schema.TypeString},
-									},
-								},
-							},
+							Type:        schema.TypeList,
+							Optional:    true,
+							Description: "List of Group IDs to Include",
+							Elem:        &schema.Schema{Type: schema.TypeString},
 						},
 						"authprovider": {
-							Type:        schema.TypeSet,
+							Type:        schema.TypeList,
 							Optional:    true,
 							Description: "Authentication Provider for the Policy",
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
 									"provider": {
-										Type:        schema.TypeString,
-										Optional:    true,
-										Default:     "OKTA",
-										ValidateFunc: validation.StringInSlice([]string{"OKTA", "ACTIVE_DIRECTORY"}, false),
-										Description: "Authentication Provider: OKTA or ACTIVE_DIRECTORY",
+										Type:     schema.TypeString,
+										Optional: true,
+										Default:  "OKTA",
+										// ValidateFunc: validation.StringInSlice([]string{"OKTA", "ACTIVE_DIRECTORY"}, false),
+										ValidateFunc: validation.StringInSlice([]string{"OKTA"}, false),
+										Description:  "Authentication Provider: OKTA or ACTIVE_DIRECTORY, Active Directory currently unsupported",
 									},
 									"include": {
 										Type:        schema.TypeList,
@@ -135,9 +94,9 @@ func resourcePolicies() *schema.Resource {
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"password": {
-							Type:          schema.TypeSet,
-							Optional:      true,
-							Description:   "User Password Policies",
+							Type:        schema.TypeSet,
+							Optional:    true,
+							Description: "User Password Policies",
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
 									"minlength": {
@@ -147,32 +106,32 @@ func resourcePolicies() *schema.Resource {
 										Description: "Minimum password length",
 									},
 									"minlowercase": {
-										Type:        schema.TypeInt,
-										Optional:    true,
-										Default:     1,
+										Type:         schema.TypeInt,
+										Optional:     true,
+										Default:      1,
 										ValidateFunc: validation.IntBetween(0, 1),
-										Description: "If a password must contain at least one lower case letter: 0 = no, 1 = yes",
+										Description:  "If a password must contain at least one lower case letter: 0 = no, 1 = yes",
 									},
 									"minuppercase": {
-										Type:        schema.TypeInt,
-										Optional:    true,
-										Default:     1,
+										Type:         schema.TypeInt,
+										Optional:     true,
+										Default:      1,
 										ValidateFunc: validation.IntBetween(0, 1),
-										Description: "If a password must contain at least one upper case letter: 0 = no, 1 = yes",
+										Description:  "If a password must contain at least one upper case letter: 0 = no, 1 = yes",
 									},
 									"minnumber": {
-										Type:        schema.TypeInt,
-										Optional:    true,
-										Default:     1,
+										Type:         schema.TypeInt,
+										Optional:     true,
+										Default:      1,
 										ValidateFunc: validation.IntBetween(0, 1),
-										Description: "If a password must contain at least one number: 0 = no, 1 = yes",
+										Description:  "If a password must contain at least one number: 0 = no, 1 = yes",
 									},
 									"minsymbol": {
-										Type:        schema.TypeInt,
-										Optional:    true,
-										Default:     1,
+										Type:         schema.TypeInt,
+										Optional:     true,
+										Default:      1,
 										ValidateFunc: validation.IntBetween(0, 1),
-										Description: "If a password must contain at least one symbol (!@#$%^&*): 0 = no, 1 = yes",
+										Description:  "If a password must contain at least one symbol (!@#$%^&*): 0 = no, 1 = yes",
 									},
 									"excludeusername": {
 										Type:        schema.TypeBool,
@@ -235,11 +194,11 @@ func resourcePolicies() *schema.Resource {
 										Description: "If a user should be informed when their account is locked",
 									},
 									"recoveryquestion": {
-										Type:        schema.TypeString,
-										Optional:    true,
-										Default:     "ACTIVE",
+										Type:         schema.TypeString,
+										Optional:     true,
+										Default:      "ACTIVE",
 										ValidateFunc: validation.StringInSlice([]string{"ACTIVE", "INACTIVE"}, false),
-										Description: "Enable or Disable the recovery question: ACTIVE or INACTIVE",
+										Description:  "Enable or Disable the recovery question: ACTIVE or INACTIVE",
 									},
 									"questionminlength": {
 										Type:        schema.TypeInt,
@@ -254,11 +213,11 @@ func resourcePolicies() *schema.Resource {
 										Description: "Lifetime in minutes of the recovery email token",
 									},
 									"smsrecovery": {
-										Type:        schema.TypeString,
-										Optional:    true,
-										Default:     "INACTIVE",
+										Type:         schema.TypeString,
+										Optional:     true,
+										Default:      "INACTIVE",
 										ValidateFunc: validation.StringInSlice([]string{"ACTIVE", "INACTIVE"}, false),
-										Description: "If SMS password recovery is enabled or disabled: ACTIVE or INACTIVE",
+										Description:  "If SMS password recovery is enabled or disabled: ACTIVE or INACTIVE",
 									},
 									"skipunlock": {
 										Type:        schema.TypeBool,
@@ -278,7 +237,6 @@ func resourcePolicies() *schema.Resource {
 
 func resourcePolicyCreate(d *schema.ResourceData, m interface{}) error {
 	log.Printf("[INFO] Creating Policy %v", d.Get("name").(string))
-	client := m.(*Config).oktaClient
 
 	exists, _, err := policyExists(d, m)
 	if err != nil {
@@ -289,48 +247,15 @@ func resourcePolicyCreate(d *schema.ResourceData, m interface{}) error {
 	} else {
 		switch d.Get("type").(string) {
 		case "PASSWORD":
-			template := client.Policies.PasswordPolicy()
-			template.Name = d.Get("name").(string)
-			template.Description = d.Get("description").(string)
-			template.Type = d.Get("type").(string)
-			template.Priority = d.Get("priority").(int)
-			template.System = d.Get("system").(bool)
-			template.Conditions.AuthProvider.Provider = "OKTA"                    // default
-			template.Settings.Recovery.Factors.OktaEmail.Status = "ACTIVE"        // default, read only
-			template.Settings.Recovery.Factors.RecoveryQuestion.Status = "ACTIVE" // default
-
-			newPolicy, _, err := client.Policies.CreatePolicy(template)
+			policyPassword("", "create", d, m)
 			if err != nil {
-				return fmt.Errorf("[ERROR] Error Creating Policy: %v", err)
-			}
-			log.Printf("[INFO] Okta Policy Created: %+v", newPolicy)
-
-			if d.Get("status").(string) == "INACTIVE" {
-				_, err = client.Policies.DeactivatePolicy(newPolicy.ID)
-				if err != nil {
-					return fmt.Errorf("[ERROR] Error Deactivating Policy: %v", err)
-				}
+				return err
 			}
 
 		case "OKTA_SIGN_ON":
-			template := client.Policies.SignOnPolicy()
-			template.Name = d.Get("name").(string)
-			template.Description = d.Get("description").(string)
-			template.Type = d.Get("type").(string)
-			template.Priority = d.Get("priority").(int)
-			template.System = d.Get("system").(bool)
-
-			newPolicy, _, err := client.Policies.CreatePolicy(template)
+			policySignOn("", "create", d, m)
 			if err != nil {
-				return fmt.Errorf("[ERROR] Error Creating Policy: %v", err)
-			}
-			log.Printf("[INFO] Okta Policy Created: %+v", newPolicy)
-
-			if d.Get("status").(string) == "INACTIVE" {
-				_, err = client.Policies.DeactivatePolicy(newPolicy.ID)
-				if err != nil {
-					return fmt.Errorf("[ERROR] Error Deactivating Policy: %v", err)
-				}
+				return err
 			}
 
 		case "MFA_ENROLL":
@@ -364,7 +289,7 @@ func resourcePolicyRead(d *schema.ResourceData, m interface{}) error {
 
 func resourcePolicyUpdate(d *schema.ResourceData, m interface{}) error {
 	log.Printf("[INFO] Update Policy %v", d.Get("name").(string))
-	client := m.(*Config).oktaClient
+	d.Partial(true)
 
 	exists, id, err := policyExists(d, m)
 	if err != nil {
@@ -373,62 +298,15 @@ func resourcePolicyUpdate(d *schema.ResourceData, m interface{}) error {
 	if exists == true {
 		switch d.Get("type").(string) {
 		case "PASSWORD":
-			template := client.Policies.PasswordPolicy()
-			template.Name = d.Get("name").(string)
-			template.Description = d.Get("description").(string)
-			template.Type = d.Get("type").(string)
-			template.Status = d.Get("status").(string)
-			template.Priority = d.Get("priority").(int)
-			template.System = d.Get("system").(bool)
-			template.Conditions.AuthProvider.Provider = "OKTA"                    // default
-			template.Settings.Recovery.Factors.OktaEmail.Status = "ACTIVE"        // default
-			template.Settings.Recovery.Factors.RecoveryQuestion.Status = "ACTIVE" // default
-
-			updatePolicy, _, err := client.Policies.UpdatePolicy(id, template)
+			policyPassword(id, "create", d, m)
 			if err != nil {
-				return fmt.Errorf("[ERROR] Error Updating Policy: %v", err)
-			}
-			log.Printf("[INFO] Okta Policy Updated: %+v", updatePolicy)
-
-			if d.Get("status").(string) == "ACTIVE" {
-				_, err = client.Policies.ActivatePolicy(updatePolicy.ID)
-				if err != nil {
-					return fmt.Errorf("[ERROR] Error Activating Policy: %v", err)
-				}
-			}
-			if d.Get("status").(string) == "INACTIVE" {
-				_, err = client.Policies.DeactivatePolicy(updatePolicy.ID)
-				if err != nil {
-					return fmt.Errorf("[ERROR] Error Deactivating Policy: %v", err)
-				}
+				return err
 			}
 
 		case "OKTA_SIGN_ON":
-			template := client.Policies.SignOnPolicy()
-			template.Name = d.Get("name").(string)
-			template.Description = d.Get("description").(string)
-			template.Type = d.Get("type").(string)
-			template.Status = d.Get("status").(string)
-			template.Priority = d.Get("priority").(int)
-			template.System = d.Get("system").(bool)
-
-			updatePolicy, _, err := client.Policies.UpdatePolicy(id, template)
+			policySignOn("id", "create", d, m)
 			if err != nil {
-				return fmt.Errorf("[ERROR] Error Updating Policy: %v", err)
-			}
-			log.Printf("[INFO] Okta Policy Updated: %+v", updatePolicy)
-
-			if d.Get("status").(string) == "ACTIVE" {
-				_, err = client.Policies.ActivatePolicy(updatePolicy.ID)
-				if err != nil {
-					return fmt.Errorf("[ERROR] Error Activating Policy: %v", err)
-				}
-			}
-			if d.Get("status").(string) == "INACTIVE" {
-				_, err = client.Policies.DeactivatePolicy(updatePolicy.ID)
-				if err != nil {
-					return fmt.Errorf("[ERROR] Error Deactivating Policy: %v", err)
-				}
+				return err
 			}
 
 		case "MFA_ENROLL":
@@ -440,6 +318,7 @@ func resourcePolicyUpdate(d *schema.ResourceData, m interface{}) error {
 	} else {
 		return fmt.Errorf("[ERROR] Error Policy not found in Okta: %v", err)
 	}
+	d.Partial(false)
 
 	return nil
 }
@@ -479,4 +358,121 @@ func policyExists(d *schema.ResourceData, m interface{}) (bool, string, error) {
 		}
 	}
 	return false, "", nil
+}
+
+func policyActivate(id string, d *schema.ResourceData, m interface{}) error {
+	client := m.(*Config).oktaClient
+
+	if d.Get("status").(string) == "ACTIVE" {
+		_, err := client.Policies.ActivatePolicy(id)
+		if err != nil {
+			return fmt.Errorf("[ERROR] Error Activating Policy: %v", err)
+		}
+	}
+	if d.Get("status").(string) == "INACTIVE" {
+		_, err := client.Policies.DeactivatePolicy(id)
+		if err != nil {
+			return fmt.Errorf("[ERROR] Error Deactivating Policy: %v", err)
+		}
+	}
+	return nil
+}
+
+func policyPassword(id string, action string, d *schema.ResourceData, m interface{}) error {
+	client := m.(*Config).oktaClient
+
+	template := client.Policies.PasswordPolicy()
+	template.Name = d.Get("name").(string)
+	template.Description = d.Get("description").(string)
+	template.Type = d.Get("type").(string)
+	template.Status = d.Get("status").(string)
+	template.Priority = d.Get("priority").(int)
+	template.System = d.Get("system").(bool)
+	template.Conditions.AuthProvider.Provider = "OKTA"                    // default
+	template.Settings.Recovery.Factors.OktaEmail.Status = "ACTIVE"        // default, read only
+	template.Settings.Recovery.Factors.RecoveryQuestion.Status = "ACTIVE" // defaulta
+
+	//for _, cond := range d.Get("conditions").([]interface{}) {
+	//	// TODO: authprovider support not included until Active Directory support included
+	//	vals := cond.(map[string]interface{})
+	//	if attr, ok := vals["groups"]; ok {
+	//		include := make([]string, 0)
+	//		for _, id := range attr {
+	//			include = append(include, id.(string))
+	//			return fmt.Errorf("%+v", include)
+	//		}
+	//	}
+	//}
+
+	switch action {
+	case "create":
+		policy, _, err := client.Policies.CreatePolicy(template)
+		if err != nil {
+			return fmt.Errorf("[ERROR] Error Creating Policy: %v", err)
+		}
+		log.Printf("[INFO] Okta Policy Created: %+v", policy)
+
+		err = policyActivate(policy.ID, d, m)
+		if err != nil {
+			return err
+		}
+
+	case "update":
+		policy, _, err := client.Policies.UpdatePolicy(id, template)
+		if err != nil {
+			return fmt.Errorf("[ERROR] Error Updating Policy: %v", err)
+		}
+		log.Printf("[INFO] Okta Policy Updated: %+v", policy)
+
+		err = policyActivate(policy.ID, d, m)
+		if err != nil {
+			return err
+		}
+
+	default:
+		return fmt.Errorf("[ERROR] policyPassword action only supports \"create\" and \"update\"")
+	}
+	return nil
+}
+
+func policySignOn(id string, action string, d *schema.ResourceData, m interface{}) error {
+	client := m.(*Config).oktaClient
+
+	template := client.Policies.SignOnPolicy()
+	template.Name = d.Get("name").(string)
+	template.Description = d.Get("description").(string)
+	template.Type = d.Get("type").(string)
+	template.Status = d.Get("status").(string)
+	template.Priority = d.Get("priority").(int)
+	template.System = d.Get("system").(bool)
+
+	switch action {
+	case "create":
+		policy, _, err := client.Policies.CreatePolicy(template)
+		if err != nil {
+			return fmt.Errorf("[ERROR] Error Creating Policy: %v", err)
+		}
+		log.Printf("[INFO] Okta Policy Created: %+v", policy)
+
+		err = policyActivate(policy.ID, d, m)
+		if err != nil {
+			return err
+		}
+
+	case "update":
+		policy, _, err := client.Policies.UpdatePolicy(id, template)
+		if err != nil {
+			return fmt.Errorf("[ERROR] Error Updating Policy: %v", err)
+		}
+		log.Printf("[INFO] Okta Policy Updated: %+v", policy)
+
+		err = policyActivate(policy.ID, d, m)
+		if err != nil {
+			return err
+		}
+
+	default:
+		return fmt.Errorf("[ERROR] policySignOn action only supports \"create\" and \"update\"")
+	}
+	return nil
 }
