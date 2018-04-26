f84a732096c740eae38e2deeffa494275da527cb
Melissa Meyer <mmeyer@articulate.com>
2018-04-26 15:09:16 +1000

    refactor to prepare for suctom profile fields
    remove unecessary json marshalling
    TF id is now the Okta id
    add partial for users update
    move user template creation to own function

--- okta/resource_users.go
+++ okta/resource_users.go
@@ -1,7 +1,6 @@
 package okta
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 
@@ -26,16 +25,20 @@ func resourceUsers() *schema.Resource {
 				Required:    true,
 				Description: "User last name",
 			},
-
 			"email": &schema.Schema{
 				Type:        schema.TypeString,
 				Required:    true,
 				Description: "User email address",
 			},
+			"login": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "User Okta login",
+			},
 			"role": &schema.Schema{
 				Type:        schema.TypeString,
 				Optional:    true,
-				Description: "User okta role",
+				Description: "User Okta role",
 			},
 		},
 	}
@@ -45,53 +48,22 @@ func resourceUserCreate(d *schema.ResourceData, m interface{}) error {
 	log.Printf("[INFO] Creating User %v", d.Get("email").(string))
 	client := m.(*Config).oktaClient
 
-	_, _, err := client.Users.GetByID(d.Get("email").(string))
-	switch {
-
-	// only create the user in okta if they do not already exist
-	case client.OktaErrorCode == "E0000007":
-
-		newUserTemplate := client.Users.NewUser()
-		newUserTemplate.Profile.FirstName = d.Get("firstname").(string)
-		newUserTemplate.Profile.LastName = d.Get("lastname").(string)
-		newUserTemplate.Profile.Login = d.Get("email").(string)
-		newUserTemplate.Profile.Email = newUserTemplate.Profile.Login
-
-		_, err = json.Marshal(newUserTemplate)
+	filter := client.Users.UserListFilterOptions()
+	filter.EmailEqualTo = d.Get("email").(string)
+	newUser, _, err := client.Users.ListWithFilter(&filter)
+	if len(newUser) == 0  {
+		userTemplate("create", d, m)
 		if err != nil {
-			return fmt.Errorf("[ERROR] Error json formatting new user template: %v", err)
+			return err
 		}
-
-		// activate user but send an email to set their password
-		// okta user status will be "Password Reset" until they complete
-		// the okta signup process
-		createNewUserAsActive := true
-
-		newUser, _, err := client.Users.Create(newUserTemplate, createNewUserAsActive)
-		if err != nil {
-			return fmt.Errorf("[ERROR] Error Creating User: %v", err)
-		}
-		log.Printf("[INFO] Okta User Created: %+v", newUser)
-
-		// assign the user a role, if specified
-		if d.Get("role").(string) != "" {
-			log.Printf("[INFO] Assigning role: " + d.Get("role").(string))
-			_, err := client.Users.AssignRole(newUser.ID, d.Get("role").(string))
-			if err != nil {
-				return fmt.Errorf("[ERROR] Error assigning role to user: %v", err)
-			}
-		}
-
-	case err != nil:
-		return fmt.Errorf("[ERROR] Error GetByID: %v", err)
-
-	default:
-		log.Printf("[INFO] User %v already exists in Okta. Adding to Terraform.",
-			d.Get("email").(string))
+		return nil
 	}
-
+	if len(newUser) > 1  {
+		return fmt.Errorf("[ERROR] Retrieved more than one Okta user for the email %v", d.Get("email").(string))
+	}
+	log.Printf("[INFO] User already exists in Okta. Adding to Terraform")
 	// add the user resource to terraform
-	d.SetId(d.Get("email").(string))
+	d.SetId(newUser[0].ID)
 
 	return nil
 }
@@ -100,7 +72,7 @@ func resourceUserRead(d *schema.ResourceData, m interface{}) error {
 	log.Printf("[INFO] List User %v", d.Get("email").(string))
 	client := m.(*Config).oktaClient
 
-	userList, _, err := client.Users.GetByID(d.Get("email").(string))
+	_, _, err := client.Users.GetByID(d.Id())
 	if err != nil {
 		// if the user does not exist in okta, delete from terraform state
 		if client.OktaErrorCode == "E0000007" {
@@ -110,22 +82,16 @@ func resourceUserRead(d *schema.ResourceData, m interface{}) error {
 			return fmt.Errorf("[ERROR] Error GetByID: %v", err)
 		}
 	} else {
-		userRoles, _, err := client.Users.ListRoles(userList.ID)
+		userRoles, _, err := client.Users.ListRoles(d.Id())
 		if err != nil {
 			return fmt.Errorf("[ERROR] Error listing user role: %v", err)
 		}
 		if userRoles != nil {
 			if len(userRoles.Role) > 1 {
-				return fmt.Errorf("[ERROR] User has more than one role. This terraform provider presently only supports a single role per user. Please review the role assignments in Okta for this user.")
-			}
-			log.Printf("[INFO] List Role: %+v", userRoles.Role[0])
-			// if they differ, change terraform user state role to okta user role
-			if d.Get("role").(string) != userRoles.Role[0].Type {
-				d.Set("role", userRoles.Role[0].Type)
+				return fmt.Errorf("[ERROR] User has more than one role. This terraform provider presently only supports a single role per user. Please review the user's role assignments in Okta.")
 			}
 		}
 	}
-	log.Printf("[INFO] List User: %+v", userList)
 
 	return nil
 }
@@ -133,73 +99,17 @@ func resourceUserRead(d *schema.ResourceData, m interface{}) error {
 func resourceUserUpdate(d *schema.ResourceData, m interface{}) error {
 	log.Printf("[INFO] Update User %v", d.Get("email").(string))
 	client := m.(*Config).oktaClient
+	d.Partial(true)
 
-	userList, _, err := client.Users.GetByID(d.Get("email").(string))
+	_, _, err := client.Users.GetByID(d.Id())
 	if err != nil {
 		return fmt.Errorf("[ERROR] Error GetByID: %v", err)
 	}
-	log.Printf("[INFO] User List: %+v", userList)
-
-	if d.HasChange("firstname") || d.HasChange("lastname") {
-		updateUserTemplate := client.Users.NewUser()
-		updateUserTemplate.Profile.FirstName = d.Get("firstname").(string)
-		updateUserTemplate.Profile.LastName = d.Get("lastname").(string)
-		updateUserTemplate.Profile.Login = d.Get("email").(string)
-		updateUserTemplate.Profile.Email = updateUserTemplate.Profile.Login
-
-		_, err = json.Marshal(updateUserTemplate)
-		if err != nil {
-			return fmt.Errorf("[ERROR] Error json formatting update user template: %v", err)
-		}
-
-		// update the user in okta
-		updateUser, _, err := client.Users.Update(updateUserTemplate, d.Get("email").(string))
-		if err != nil {
-			return fmt.Errorf("[ERROR] Error Updating User: %v", err)
-		}
-		log.Printf("[INFO] Okta User Updated: %+v", updateUser)
-
-		// update the user resource in terraform
-		d.Set("firstname", d.Get("firstname").(string))
-		d.Set("lastname", d.Get("lastname").(string))
+	userTemplate("update", d, m)
+	if err != nil {
+		return err
 	}
-
-	if d.HasChange("role") {
-		userRoles, _, err := client.Users.ListRoles(userList.ID)
-		if err != nil {
-			return fmt.Errorf("[ERROR] Error listing user role: %v", err)
-		}
-		switch {
-
-		case userRoles == nil && d.Get("role").(string) != "":
-			log.Printf("[INFO] Assigning role: " + d.Get("role").(string))
-			_, err = client.Users.AssignRole(userList.ID, d.Get("role").(string))
-			if err != nil {
-				return fmt.Errorf("[ERROR] Error assigning role to user: %v", err)
-			}
-
-		case userRoles != nil && d.Get("role").(string) != "":
-			log.Printf("[INFO] Changing role: " + d.Get("role").(string) + " to " + userRoles.Role[0].Type)
-			_, err = client.Users.UnAssignRole(userList.ID, userRoles.Role[0].ID)
-			if err != nil {
-				return fmt.Errorf("[ERROR] Error removing role from user: %v", err)
-			}
-			_, err = client.Users.AssignRole(userList.ID, d.Get("role").(string))
-			if err != nil {
-				return fmt.Errorf("[ERROR] Error assigning role to user: %v", err)
-			}
-
-		case userRoles != nil && d.Get("role").(string) == "":
-			log.Printf("[INFO] Removing role: " + d.Get("role").(string))
-			_, err = client.Users.UnAssignRole(userList.ID, userRoles.Role[0].ID)
-			if err != nil {
-				return fmt.Errorf("[ERROR] Error removing role from user: %v", err)
-			}
-
-		default:
-			return fmt.Errorf("User role changed but Terraform was unable to apply. Please investigate.")
-		}
-	}
+	d.Partial(false)
 
 	return nil
 }
@@ -208,28 +118,97 @@ func resourceUserDelete(d *schema.ResourceData, m interface{}) error {
 	log.Printf("[INFO] Delete User %v", d.Get("email").(string))
 	client := m.(*Config).oktaClient
 
-	userList, _, err := client.Users.GetByID(d.Get("email").(string))
+	userList, _, err := client.Users.GetByID(d.Id())
 	if err != nil {
 		return fmt.Errorf("[ERROR] Error GetByID: %v", err)
 	}
-
 	// must deactivate the user before deletion
 	if userList.Status != "DEPROVISIONED" {
-		_, err := client.Users.Deactivate(d.Get("email").(string))
+		_, err := client.Users.Deactivate(d.Id())
 		if err != nil {
 			return fmt.Errorf("[ERROR] Error Deactivating user: %v", err)
 		}
 	}
-
-	// now! delete the user
-	deleteUser, err := client.Users.Delete(d.Get("email").(string))
+	// delete the user
+	_, err = client.Users.Delete(d.Id())
 	if err != nil {
 		return fmt.Errorf("[ERROR] Error Deleting user: %v", err)
 	}
-	log.Printf("[INFO] Okta User Deleted: %+v", deleteUser)
-
 	// delete the user resource from terraform
 	d.SetId("")
+
+	return nil
+}
+
+func userTemplate(action string, d *schema.ResourceData, m interface{}) error {
+	client := m.(*Config).oktaClient
+
+	template := client.Users.NewUser()
+	template.Profile.FirstName = d.Get("firstname").(string)
+	template.Profile.LastName = d.Get("lastname").(string)
+	template.Profile.Email = d.Get("email").(string)
+	if _, ok := d.GetOk("login"); ok {
+		template.Profile.Login = d.Get("login").(string)
+	} else {
+		template.Profile.Login = d.Get("email").(string)
+	}
+
+	switch action {
+	case "create":
+		// activate user but send an email to set their password
+		// okta user status will be "Password Reset" until they complete
+		// the okta signup process
+		createNewUserAsActive := true
+
+		newUser, _, err := client.Users.Create(template, createNewUserAsActive)
+		if err != nil {
+			return fmt.Errorf("[ERROR] Error Creating User: %v", err)
+		}
+		log.Printf("[INFO] Okta User Created: %+v", newUser)
+
+		// assign the user a role, if specified
+		if _, ok := d.GetOk("role"); ok {
+			log.Printf("[INFO] Assigning role: " + d.Get("role").(string))
+			_, err := client.Users.AssignRole(newUser.ID, d.Get("role").(string))
+			if err != nil {
+				return fmt.Errorf("[ERROR] Error assigning role to user: %v", err)
+			}
+		}
+		// add the user resource to terraform
+		d.SetId(newUser.ID)
+
+	case "update":
+		updateUser, _, err := client.Users.Update(template, d.Id())
+		if err != nil {
+			return fmt.Errorf("[ERROR] Error Updating User: %v", err)
+		}
+		log.Printf("[INFO] Okta User Updated: %+v", updateUser)
+
+		userRoles, _, err := client.Users.ListRoles(d.Id())
+		if err != nil {
+			return fmt.Errorf("[ERROR] Error listing user role: %v", err)
+		}
+
+		if d.HasChange("role") {
+			if userRoles != nil {
+				log.Printf("[INFO] Removing role: " + userRoles.Role[0].Type)
+				_, err = client.Users.UnAssignRole(d.Id(), userRoles.Role[0].ID)
+				if err != nil {
+					return fmt.Errorf("[ERROR] Error removing role from user: %v", err)
+				}
+			}
+			if _, ok := d.GetOk("role"); ok {
+				log.Printf("[INFO] Assigning role: " + d.Get("role").(string))
+				_, err = client.Users.AssignRole(d.Id(), d.Get("role").(string))
+				if err != nil {
+					return fmt.Errorf("[ERROR] Error assigning role to user: %v", err)
+				}
+			}
+		}
+
+	default:
+		return fmt.Errorf("[ERROR] userTemplate action only supports \"create\" and \"update\"")
+	}
 
 	return nil
 }
--- okta/resource_users_test.go
+++ okta/resource_users_test.go
@@ -13,35 +13,9 @@ import (
 // Witiz1932@teleworm.us is a fake email address created at fakemailgenerator.com
 // view inbox: http://www.fakemailgenerator.com/inbox/teleworm.us/witiz1932/
 
-func TestAccOktaUsers_create(t *testing.T) {
-	resourceName := "okta_users.test"
+func TestAccOktaUsers(t *testing.T) {
 	ri := acctest.RandInt()
-
-	config := testOktaUsers(ri)
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testOktaUsersDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: config,
-				Check: resource.ComposeTestCheckFunc(
-					testOktaUsersExists(resourceName),
-					resource.TestCheckResourceAttr(resourceName, "firstname", "terraform_acc_test"),
-					resource.TestCheckResourceAttr(resourceName, "lastname", strconv.Itoa(ri)),
-					resource.TestCheckResourceAttr(resourceName, "email", "Witiz1932@teleworm.us"),
-					resource.TestCheckResourceAttr(resourceName, "role", "SUPER_ADMIN"),
-				),
-			},
-		},
-	})
-}
-
-func TestAccOktaUsers_update(t *testing.T) {
-	resourceName := "okta_users.test"
-	ri := acctest.RandInt()
-
+	resourceName := "okta_users.test-" + strconv.Itoa(ri)
 	config := testOktaUsers(ri)
 	updatedConfig := testOktaUsers_updated(ri)
 
@@ -54,7 +28,7 @@ func TestAccOktaUsers_update(t *testing.T) {
 				Config: config,
 				Check: resource.ComposeTestCheckFunc(
 					testOktaUsersExists(resourceName),
-					resource.TestCheckResourceAttr(resourceName, "firstname", "terraform_acc_test"),
+					resource.TestCheckResourceAttr(resourceName, "firstname", "testAcc"),
 					resource.TestCheckResourceAttr(resourceName, "lastname", strconv.Itoa(ri)),
 					resource.TestCheckResourceAttr(resourceName, "email", "Witiz1932@teleworm.us"),
 					resource.TestCheckResourceAttr(resourceName, "role", "SUPER_ADMIN"),
@@ -64,7 +38,7 @@ func TestAccOktaUsers_update(t *testing.T) {
 				Config: updatedConfig,
 				Check: resource.ComposeTestCheckFunc(
 					testOktaUsersExists(resourceName),
-					resource.TestCheckResourceAttr(resourceName, "firstname", "terraform_acc_test_updated"),
+					resource.TestCheckResourceAttr(resourceName, "firstname", "testAcc_updated"),
 					resource.TestCheckResourceAttr(resourceName, "lastname", strconv.Itoa(ri)),
 					resource.TestCheckResourceAttr(resourceName, "email", "Witiz1932@teleworm.us"),
 					resource.TestCheckResourceAttr(resourceName, "role", "READ_ONLY_ADMIN"),
@@ -75,9 +49,8 @@ func TestAccOktaUsers_update(t *testing.T) {
 }
 
 func TestAccOktaUsersRole_delete(t *testing.T) {
-	resourceName := "okta_users.test"
 	ri := acctest.RandInt()
-
+	resourceName := "okta_users.test-" + strconv.Itoa(ri)
 	config := testOktaUsers(ri)
 	updatedConfig := testOktaUsersRole_delete(ri)
 
@@ -90,7 +63,7 @@ func TestAccOktaUsersRole_delete(t *testing.T) {
 				Config: config,
 				Check: resource.ComposeTestCheckFunc(
 					testOktaUsersExists(resourceName),
-					resource.TestCheckResourceAttr(resourceName, "firstname", "terraform_acc_test"),
+					resource.TestCheckResourceAttr(resourceName, "firstname", "testAcc"),
 					resource.TestCheckResourceAttr(resourceName, "lastname", strconv.Itoa(ri)),
 					resource.TestCheckResourceAttr(resourceName, "email", "Witiz1932@teleworm.us"),
 					resource.TestCheckResourceAttr(resourceName, "role", "SUPER_ADMIN"),
@@ -100,7 +73,7 @@ func TestAccOktaUsersRole_delete(t *testing.T) {
 				Config: updatedConfig,
 				Check: resource.ComposeTestCheckFunc(
 					testOktaUsersExists(resourceName),
-					resource.TestCheckResourceAttr(resourceName, "firstname", "terraform_acc_test_role_deleted"),
+					resource.TestCheckResourceAttr(resourceName, "firstname", "testAcc_role_delete"),
 					resource.TestCheckResourceAttr(resourceName, "lastname", strconv.Itoa(ri)),
 					resource.TestCheckResourceAttr(resourceName, "email", "Witiz1932@teleworm.us"),
 					resource.TestCheckResourceAttr(resourceName, "role", ""),
@@ -118,35 +91,32 @@ func testOktaUsersExists(name string) resource.TestCheckFunc {
 			return fmt.Errorf("Not found: %s", name)
 		}
 
-		email, hasEmail := rs.Primary.Attributes["email"]
-		if !hasEmail {
-			return fmt.Errorf("Error: no email found in state for resource: %s", name)
+		userID, hasID := rs.Primary.Attributes["id"]
+		if !hasID {
+			return fmt.Errorf("[ERROR] No id found in state")
 		}
-		_, hasFirstName := rs.Primary.Attributes["firstname"]
+		firstName, hasFirstName := rs.Primary.Attributes["firstname"]
 		if !hasFirstName {
-			return fmt.Errorf("Error: no firstname found in state for user: %s", email)
+			return fmt.Errorf("Error: no firstname found in state")
 		}
-		_, hasLastName := rs.Primary.Attributes["lastname"]
+		lastName, hasLastName := rs.Primary.Attributes["lastname"]
 		if !hasLastName {
-			return fmt.Errorf("Error: no lastname found in state for user: %s", email)
+			return fmt.Errorf("Error: no lastname found in state")
+		}
+
+		err := testUserExists(true, userID, firstName, lastName)
+		if err != nil {
+			return err
 		}
 
 		client := testAccProvider.Meta().(*Config).oktaClient
-
-		userList, _, err := client.Users.GetByID(email)
-		if err != nil {
-			if client.OktaErrorCode == "E0000007" {
-				return fmt.Errorf("Error: User %s does not exist", email)
-			}
-			return fmt.Errorf("Error: GetByID: %v", err)
-		}
-		userRoles, _, err := client.Users.ListRoles(userList.ID)
+		userRoles, _, err := client.Users.ListRoles(userID)
 		if err != nil {
 			return fmt.Errorf("Error: listing user role: %v", err)
 		}
-		role, _ := rs.Primary.Attributes["role"]
+		role, hasRole := rs.Primary.Attributes["role"]
 		if userRoles != nil {
-			if role == "" {
+			if !hasRole {
 				return fmt.Errorf("Error: Okta role %v exists but Terraform state role does not", userRoles.Role[0].Type)
 			}
 			if role != userRoles.Role[0].Type {
@@ -159,58 +129,82 @@ func testOktaUsersExists(name string) resource.TestCheckFunc {
 }
 
 func testOktaUsersDestroy(s *terraform.State) error {
-	client := testAccProvider.Meta().(*Config).oktaClient
-
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "okta_users" {
 			continue
 		}
 
-		email, hasEmail := rs.Primary.Attributes["email"]
-		if !hasEmail {
-			return fmt.Errorf("Error: no email found in state for user")
+		userID, hasID := rs.Primary.Attributes["id"]
+		if !hasID {
+			return fmt.Errorf("[ERROR] No id found in state")
+		}
+		firstName, hasFirstName := rs.Primary.Attributes["firstname"]
+		if !hasFirstName {
+			return fmt.Errorf("Error: no firstname found in state")
+		}
+		lastName, hasLastName := rs.Primary.Attributes["lastname"]
+		if !hasLastName {
+			return fmt.Errorf("Error: no lastname found in state")
 		}
 
-		_, _, err := client.Users.GetByID(email)
+		err := testUserExists(false, userID, firstName, lastName)
 		if err != nil {
-			if client.OktaErrorCode == "E0000007" {
-				return nil
-			}
-			return fmt.Errorf("Error: GetByID: %v", err)
+			return err
 		}
-		return fmt.Errorf("User still exists: %s", email)
+		return nil
+	}
+	return nil
+}
+
+func testUserExists(expected bool, userID string, firstName string, lastName string) error {
+	client := testAccProvider.Meta().(*Config).oktaClient
+
+	exists := false
+	_, _, err := client.Users.GetByID(userID)
+	if err != nil {
+		if client.OktaErrorCode != "E0000007" {
+			return fmt.Errorf("[ERROR] Error Listing User in Okta: %v", err)
+		}
+	} else {
+		exists = true
+	}
+
+	if expected == true && exists == false {
+		return fmt.Errorf("[ERROR] User %v %v not found in Okta", firstName, lastName)
+	} else if expected == false && exists == true {
+		return fmt.Errorf("[ERROR] User %v %v still exists in Okta", firstName, lastName)
 	}
 	return nil
 }
 
 func testOktaUsers(rInt int) string {
 	return fmt.Sprintf(`
-resource "okta_users" "test" {
-  firstname = "terraform_acc_test"
+resource "okta_users" "test-%d" {
+  firstname = "testAcc"
   lastname  = "%d"
   email     = "Witiz1932@teleworm.us"
   role      = "SUPER_ADMIN"
 }
-`, rInt)
+`, rInt, rInt)
 }
 
 func testOktaUsers_updated(rInt int) string {
 	return fmt.Sprintf(`
-resource "okta_users" "test" {
-  firstname = "terraform_acc_test_updated"
+resource "okta_users" "test-%d" {
+  firstname = "testAcc_updated"
   lastname  = "%d"
   email     = "Witiz1932@teleworm.us"
   role      = "READ_ONLY_ADMIN"
 }
-`, rInt)
+`, rInt, rInt)
 }
 
 func testOktaUsersRole_delete(rInt int) string {
 	return fmt.Sprintf(`
-resource "okta_users" "test" {
-  firstname = "terraform_acc_test_role_deleted"
+resource "okta_users" "test-%d" {
+  firstname = "testAcc_role_delete"
   lastname  = "%d"
   email     = "Witiz1932@teleworm.us"
 }
-`, rInt)
+`, rInt, rInt)
 }
